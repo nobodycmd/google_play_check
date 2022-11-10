7df19a707a339af1c54ece5f9a3d11f5a0880949
Your Name <you@example.com>
2022-11-10 19:19:53 +0800

    python3 for windows and linux

--- console/controllers/QueueHelperController.php
+++ console/controllers/QueueHelperController.php
@@ -8,6 +8,14 @@ use Yii;
 require_once Yii::getAlias("@root/vendor/yiisoft/yii2-queue/src/cli/Command.php");
 require_once Yii::getAlias("@root/vendor/yiisoft/yii2-queue/src/drivers/db/Command.php");
 
+/**
+ * ./yii queue-helper/go q0
+ * ./yii queue-helper/go q1
+ * ./yii queue-helper/go q2
+ *
+ * Class QueueHelperController
+ * @package console\controllers
+ */
 class QueueHelperController extends yii\queue\db\Command
 {
     use QueueTrait;
