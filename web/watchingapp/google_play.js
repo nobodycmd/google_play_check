ab988e735f050b1d24e24cddb70e936c427e49c5
Your Name <you@example.com>
2022-11-23 15:48:12 +0800

    add nodejs search support

---
+++ web/watchingapp/google_play.js
@@ -0,0 +1,38 @@
+/**
+ * node.js 环境
+ * 
+ * npm install google-play-scraper
+ */
+
+
+ var argv = process.argv.slice(2);
+ console.log(argv)
+ var input_term = argv[0];
+ var input_limit = argv[1];
+ 
+ function save_std(data) {
+     //var json_string_results = JSON.stringify(data);
+     console.log(data);
+ }
+ 
+ 
+ function save_err(data) {
+     console.log("NodeJS> Error! " + data);
+ }
+ 
+ 
+ try {
+     var gplay = require('google-play-scraper');
+ }  catch(e) {
+     console.error(e.message);
+     console.error("google-play-scraper is probably not found. Try running `npm i google-play-scraper`.");
+     process.exit(e.code);
+ }
+ 
+ gplay.search({
+     term: input_term,
+     num: input_limit
+ }).then(save_std, save_err);
+ 
+ 
+ 
